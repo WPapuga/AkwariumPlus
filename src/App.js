1816376f2c4b643b22b85788cbc781af9273feb3
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-04-17 20:00:00 +0200

    app logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import logo from './logo.svg';                    //react logo
 import './App.css';                               //basic css
-import Button from 'react-bootstrap/Button';      
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Components/Navbar';
+import Home from './pages/Home';
+import Equipment from './pages/Equipment';
+import Fishes from './pages/Fishes';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+
 
 function App() {
+  let Component;
+  switch (window.location.pathname) {
+    case "/":
+      Component = Home;
+      break;
+    case "/wyposazenie":
+      Component = Equipment;
+      break;
+    case "/ryby":
+      Component = Fishes;
+      break;
+    case "/logowanie":
+      Component = SignIn;
+      break;
+    case "/rejestracja":
+      Component = SignUp;
+      break;
+    default:
+      Component = App;
+  }
   return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <img src="akwarium_logo.png" alt="logo"/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>Test Button</Button>
-      </header>
-    </div>
+    <Component />
   );
 }
 
