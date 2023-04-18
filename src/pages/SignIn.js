ae8f00ca16724d8d33f285f2e104ac590cc90774
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-04-19 00:22:53 +0200

    add simple forms

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,14 +1,29 @@
-import Button from 'react-bootstrap/Button';      
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';      
 
 
 export default function SignIn() {
     return (
         <div className="App">
             <header className="App-header">
+                <img className="UserImg"src="user_icon.svg" alt="logo" />
                 <p>Logowanie</p>
-                <img src="akwarium_logo.png" alt="logo"/>
-                <Button>Przycisk</Button>
             </header>
+            <body className="App-body">
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Email/Login</Form.Label>
+                        <Form.Control type="email" placeholder="e-mail" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Hasło</Form.Label>
+                        <Form.Control type="password" placeholder="hasło" />
+                    </Form.Group>
+                    <Button className="SubmitButton" variant="primary" type="submit">
+                        Zaloguj się
+                    </Button>
+                </Form>
+            </body>
         </div>
     );
 }
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,14 +1,33 @@
 import Button from 'react-bootstrap/Button';      
+import Form from 'react-bootstrap/Form';      
 
 
 export default function SignUp() {
     return (
         <div className="App">
             <header className="App-header">
+                <img className="UserImg"src="user_icon.svg" alt="logo" />
                 <p>Rejestracja</p>
-                <img src="akwarium_logo.png" alt="logo"/>
-                <Button>Przycisk</Button>
             </header>
+            <body className="App-body">
+                <Form>
+                    <Form.Group>
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="e-mail" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Login</Form.Label>
+                        <Form.Control type="email" placeholder="login" />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Hasło</Form.Label>
+                        <Form.Control type="password" placeholder="hasło" />
+                    </Form.Group>
+                    <Button className="SubmitButton" variant="primary" type="submit">
+                        Zarejestruj się
+                    </Button>
+                </Form>
+            </body>
         </div>
     );
 }
