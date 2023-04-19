2ecc84d6024a6fe82bcf81c651e07b0692836286
WPapuga <Wojtek.Papuga@yahoo.com>
2023-04-19 09:34:41 +0200

    home page motto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,12 +7,13 @@ export default function Home() {
         <div className="App">
             <header className="App-header">
                 <img src="akwarium_background.png" alt="background" style={{width: "100%", height: "auto"}}/>
+                <p>"Twoje akwarium, Twoja oaza spokoju - z nami możesz mieć pewność, że zawsze będzie w doskonałym stanie!"</p>
                 <img src="akwarium_logo.png" alt="logo"/>
                 <img src={logo} className="App-logo" alt="logo" />
                 <Button>Test Button</Button>
             </header>
             <body className="App-body">
-
+                <p>Ryby</p>
             </body>
         </div>
     );
