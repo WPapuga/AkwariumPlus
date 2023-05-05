fff872d13ef1ffb28247d83c12152fb5d45eb0aa
WPapuga <79088499+WPapuga@users.noreply.github.com>
2023-05-06 01:59:28 +0200

    hover image effect

--- app/src/Components/FishList.js
+++ app/src/Components/FishList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './FishList.css'
+import { Link } from 'react-router-dom'
 
 function FishList({ fish, loading }) {
   if(loading){
@@ -9,7 +10,7 @@ function FishList({ fish, loading }) {
     <div className="FishContainer">
         {fish.map((item) => (
             <div className='Fish' key={item.id}>
-                <img className='FishImage' src={`images/${item.image}`}></img>
+                <Link to={`/szczegolyRyby?id=${item.id}`}><img className='FishImage' src={`images/${item.image}`}></img></Link>
                 <h3 className='FishName'>{item.name}</h3>
             </div>
         ))}
